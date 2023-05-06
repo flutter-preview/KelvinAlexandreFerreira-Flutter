2b4e0c15146e78b9446913d4a27f3e27622194c7
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-05-05 21:40:59 -0400

    02 - Widgets - Column e Row

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -10,22 +10,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Stack(alignment: AlignmentDirectional.center, children: [
-        Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        Container(
-          color: Colors.blue,
-          width: 50,
-          height: 50,
-        ),
-      ]),
-    );
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
