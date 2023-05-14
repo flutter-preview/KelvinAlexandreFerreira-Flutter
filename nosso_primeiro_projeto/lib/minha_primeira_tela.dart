779bbca24149a2e0a004681fded118a5dceda9df
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-05-13 20:50:04 -0400

    05 - Boas Práticas - O que é refatoração?

--- nosso_primeiro_projeto/lib/minha_primeira_tela.dart
+++ nosso_primeiro_projeto/lib/minha_primeira_tela.dart
@@ -1,79 +1,90 @@
-// Container(
-//         color: Colors.white,
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.red,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 50,
-//                   height: 50,
-//                 )
-//               ],
-//             ),
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.red,
-//                   width: 50,
-//                   height: 50,
-//                 )
-//               ],
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Container(
-//                   color: Colors.cyan,
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//                 Container(
-//                   color: Colors.pinkAccent,
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//                 Container(
-//                   color: Colors.purple,
-//                   height: 50,
-//                   width: 50,
-//                 ),
-//               ],
-//             ),
-//             Container(
-//               color: Colors.amber,
-//               height: 30,
-//               width: 300,
-//               child: Text(
-//                 'Diamante Amarelo',
-//                 style: TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 28,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 print('Você apertou o botão');
-//               },
-//               child: Text('Aperte o botão!'),
-//             ),
-//           ],
-//         ),
-//       ),
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 50,
+                height: 50,
+              )
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.red,
+                width: 50,
+                height: 50,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.cyan,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.pinkAccent,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.purple,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.amber,
+            height: 30,
+            width: 300,
+            child: Text(
+              'Diamante Amarelo',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('Você apertou o botão');
+            },
+            child: Text('Aperte o botão!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
