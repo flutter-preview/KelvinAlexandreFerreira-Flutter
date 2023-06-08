d0193fc108e7955dce7f83f379bcd1d5e7fc6cb6
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2023-06-08 17:25:06 -0400

    02 - Lidando com dados - Estrutura de dados

--- nosso_primeiro_projeto/lib/data/database.dart
+++ nosso_primeiro_projeto/lib/data/database.dart
@@ -6,8 +6,18 @@ Future<Database> getDatabase() async {
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute(tabela);
+      db.execute(tableSql);
     },
     version: 1,
   );
 }
+
+const String tableSql = 'CREATE TABLE $_tablename('
+    '$_name TEXT, '
+    '$_difficulty INTERGER '
+    '$_image TEXT)';
+
+const String _tablename = 'taskTable';
+const String _name = 'name';
+const String _difficulty = 'difficulty';
+const String _image = 'image';
